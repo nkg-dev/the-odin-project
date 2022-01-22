28158a3d1e0aeab92626b4e094c53c27ef3e883d
Neil Ghoshal <nkg718@gmail.com>
2022-01-22 15:52:53 -0500

    Added computerPlay function

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,1 +1,14 @@
 'use strict';
+function computerPlay() {
+	// hardcoding * 3 (rather than '* max' since it's rock-paper-scissors
+	let num = Math.floor(Math.random() * 3);
+	if (num === 0) {
+		return 'Rock';
+	} else if (num === 1) {
+		return 'Paper';
+	} else {
+		return 'Scissors';
+	}
+}
+
+console.log(computerPlay());
