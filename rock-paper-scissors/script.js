b7be85477ea96959ef44aa4a1c73b945653f49a3
Neil Ghoshal <nkg718@gmail.com>
2022-02-04 12:52:06 -0500

    Added some work on looping to play 5 games but not done yet

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -64,30 +64,86 @@ let u = userPlays();
 function gamePlay(computer, user) {
 	let cPlay = computer;
 	let uPlay = user;
+	let gameCounter = 5;
 	console.log('Computer played ' + cPlay);
 	console.log('You played ' + uPlay);
-	if (uPlay === cPlay) {
-		console.log("It's a draw :|");
-		console.log('Draw');
-	} else if (uPlay == 'Rock' && cPlay == 'Scissors') {
-		console.log('Rock beats Scissors! You win!');
-		console.log('User');
-	} else if (uPlay == 'Scissors' && cPlay == 'Paper') {
-		console.log('Scissors beats Paper! You win!');
-		console.log('User');
-	} else if (uPlay == 'Paper' && cPlay == 'Rock') {
-		console.log('Paper beats Rock! You win!');
-		console.log('User');
-	} else if (uPlay == 'Rock' && cPlay == 'Paper') {
-		console.log('Paper beats Rock. You lose. :(');
-		console.log('Comp');
-	} else if (uPlay == 'Paper' && cPlay == 'Scissors') {
-		console.log('Scissors beats Paper. You lose. :(');
-		console.log('Comp');
-	} else if (uPlay == 'Scissors' && cPlay == 'Rock') {
-		console.log('Rock beats Scissors. You lose. :(');
-		console.log('Comp');
+	while (gameCounter > 0) {
+		if (uPlay === cPlay) {
+			console.log("It's a draw :|");
+			console.log('Draw');
+			gameCounter--;
+			return keepScore('Draw');
+		} else if (uPlay == 'Rock' && cPlay == 'Scissors') {
+			console.log('Rock beats Scissors! You win!');
+			console.log('User');
+			gameCounter--;
+			return keepScore('User');
+		} else if (uPlay == 'Scissors' && cPlay == 'Paper') {
+			console.log('Scissors beats Paper! You win!');
+			console.log('User');
+			gameCounter--;
+			return keepScore('User');
+		} else if (uPlay == 'Paper' && cPlay == 'Rock') {
+			console.log('Paper beats Rock! You win!');
+			console.log('User');
+			gameCounter--;
+			return keepScore('User');
+		} else if (uPlay == 'Rock' && cPlay == 'Paper') {
+			console.log('Paper beats Rock. You lose. :(');
+			console.log('Comp');
+			gameCounter--;
+			return keepScore('Comp');
+		} else if (uPlay == 'Paper' && cPlay == 'Scissors') {
+			console.log('Scissors beats Paper. You lose. :(');
+			console.log('Comp');
+			gameCounter--;
+			return keepScore('Comp');
+		} else if (uPlay == 'Scissors' && cPlay == 'Rock') {
+			console.log('Rock beats Scissors. You lose. :(');
+			console.log('Comp');
+			gameCounter--;
+			return keepScore('Comp');
+		}
 	}
 }
 
-gamePlay(c, u);
+function keepScore(result) {
+	let u = 0,
+		c = 0,
+		d = 0;
+	switch (result) {
+		case 'User':
+			u++;
+			console.log(`User: ${u}; Computer: ${c}; Draw: ${d}`);
+			break;
+		case 'Comp':
+			c++;
+			console.log(`User: ${u}; Computer: ${c}; Draw: ${d}`);
+			break;
+		case 'Draw':
+			d++;
+			console.log(`User: ${u}; Computer: ${c}; Draw: ${d}`);
+			break;
+	}
+	// i++;
+	// return i++;
+}
+
+// function gameTracker() {
+// 	let num;
+// 	num = num + 1;
+// 	return num;
+// }
+
+function letsPlay() {
+	let gameCounter = 5;
+	while (gameCounter > 0) {
+		console.log('Games left ' + gameCounter);
+		gamePlay(c, u);
+		gameCounter--;
+	}
+}
+
+letsPlay();
+
+//gamePlay(c, u);
