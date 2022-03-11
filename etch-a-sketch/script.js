9970bd24f4f091cb81409cdc4108666318cb9e2f
Neil G <95941773+nkg-dev@users.noreply.github.com>
2022-03-10 22:12:17 -0500

    Started Reset function

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -5,7 +5,7 @@
 // let divCount = 256;
 
 // let divsPerSide = Math.floor(divCount ** (1 / 2));
-let divsPerSide = 16;
+let divsPerSide = 80;
 let divCount = divsPerSide ** 2;
 let divWidth = Math.floor(960 / divsPerSide);
 console.log(`DIVs per Side = ${divsPerSide}`);
@@ -20,10 +20,10 @@ function buildGrid() {
 	for (let i = 0; i < divCount; i++) {
 		const sketchDiv = document.createElement('div');
 		sketchDiv.classList.add('sketch-div');
-		let hue = Math.floor(Math.random() * 361);
+		// let hue = Math.floor(Math.random() * 361);
 		// sketchDiv.textContent = i + '–' + hue;
-		// sketchDiv.textContent = i;
-		// sketchDiv.style.color = `hsl(${hue - 180}, 100%, 100%)`;
+		// sketchDiv.textContent = hue;
+		// sketchDiv.style.color = `hsl(${hue - 180}, 75%, 50%)`;
 		// sketchDiv.style.backgroundColor = `hsl(${hue}, 75%, 50%)`;
 		sketchDiv.style.backgroundColor = 'black';
 		sketchDiv.style.opacity = 0;
@@ -33,11 +33,14 @@ function buildGrid() {
 
 buildGrid();
 
-function getDarker() {
+//************************************************************/
+//****************** Begin code for DRAWING ******************/
+//************************************************************/
+function letsDraw() {
 	// this.style.backgroundColor = 'black';
-	//*** in color theory, 'shade' is adding (% of) black to a color ***/
+	//*** As in color theory, shade adds black to the color ****/
 	let shade = Number(this.style.getPropertyValue('opacity'));
-	//*** toFixed() function returns a string, so needs Number() *******/
+	//*** toFixed() returns a string, so needs Number() ********/
 	if (Number(shade.toFixed(1)) <= 0.9) {
 		shade += 0.1;
 		console.log(`Opacity set to ${shade}`);
@@ -45,12 +48,39 @@ function getDarker() {
 	this.style.opacity = shade;
 }
 
-function getLuminance() {
-	console.log(document.activeElement.getAttribute('backgroundColor'));
+const sketchDivs = document.querySelectorAll('div.sketch-div');
+function drawingMode() {
+	for (let i = 0; i < divCount; i++) {
+		sketchDivs[i].addEventListener('mouseover', letsDraw);
+		// sketchDivs[i].removeEventListener('mouseover', getDarker);
+	}
 }
 
-const sketchDivs = document.querySelectorAll('div.sketch-div');
-for (let i = 0; i < divCount; i++) {
-	sketchDivs[i].addEventListener('mouseover', getDarker);
-	// sketchDivs[i].removeEventListener('mouseover', getDarker);
+drawingMode();
+
+//************************************************************/
+//**************** Begin code for RESET modal ****************/
+//************************************************************/
+const resetButton = document.getElementById('resetButton');
+const modalResetContainer = document.getElementById('modalResetContainer');
+const resetYes = document.getElementById('resetYes');
+const resetNo = document.getElementById('resetNo');
+
+resetButton.addEventListener('click', () => {
+	modalResetContainer.classList.add('show');
+});
+
+resetNo.addEventListener('click', () => {
+	modalResetContainer.classList.remove('show');
+});
+
+resetYes.addEventListener('click', () => {
+	resetAll();
+	modalResetContainer.classList.remove('show');
+});
+
+function resetAll() {
+	sketchArea.innerText = '';
+	buildGrid();
+	drawingMode();
 }
