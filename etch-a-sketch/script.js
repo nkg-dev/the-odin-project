761002ba0e27da1f682336aab282d7ce92333fe6
Neil G <95941773+nkg-dev@users.noreply.github.com>
2022-03-12 15:38:22 -0500

    All functionality in place

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,33 +1,22 @@
 'use strict';
 
-//** depending on how many squares you want, adjust divCount **/
-//******* should be a squared value (ie: 256 = 16 * 16) *******/
-// let divCount = 256;
-
-// let divsPerSide = Math.floor(divCount ** (1 / 2));
-let divsPerSide = 60;
-let divCount = divsPerSide ** 2;
-let divWidth = 60 / divsPerSide;
-// let divWidth = Math.floor(60 / divsPerSide);
-// sketchArea.style.gridTemplateColumns = `repeat(${divsPerSide}, ${divWidth}px)`;
-// sketchArea.style.gridTemplateRows = `repeat(${divsPerSide}, ${divWidth}px)`;
-
-console.log(`DIVs per Side = ${divsPerSide}`);
-console.log(`DIV Width = ${divWidth}rem`);
-
-const sketchArea = document.getElementById('sketchArea');
-
-sketchArea.style.gridTemplateColumns = `repeat(${divsPerSide}, ${divWidth}rem)`;
-sketchArea.style.gridTemplateRows = `repeat(${divsPerSide}, ${divWidth}rem)`;
-
 function buildGrid() {
+	const sketchArea = document.getElementById('sketchArea');
+	let divsPerSide = document.getElementById('divsPerSide').value;
+	let divCount = divsPerSide ** 2;
+	let divWidth = 60 / divsPerSide;
+
+	sketchArea.style.gridTemplateColumns = `repeat(${divsPerSide}, calc(var(--font-size) * ${divWidth})`;
+	sketchArea.style.gridTemplateRows = `repeat(${divsPerSide}, calc(var(--font-size) * ${divWidth})`;
 	sketchArea.innerText = '';
+	// console.log(`DIVs per Side = ${divsPerSide}`);
+	// console.log(`DIV Width = ${divWidth}rem`);
 	for (let i = 0; i < divCount; i++) {
-		const sketchDiv = document.createElement('div.sketch-div');
-		// sketchDiv.classList.add('sketch-div');
+		const sketchDiv = document.createElement('div');
+		sketchDiv.classList.add('sketch-div');
 		// let hue = Math.floor(Math.random() * 361);
 		// sketchDiv.textContent = i + '–' + hue;
-		// sketchDiv.textContent = i;
+		// sketchDiv.textContent = i + 1;
 		// sketchDiv.style.color = `hsl(${hue - 180}, 75%, 50%)`;
 		// sketchDiv.style.backgroundColor = `hsl(${hue}, 75%, 50%)`;
 		sketchDiv.addEventListener('mouseover', letsDraw);
@@ -43,12 +32,15 @@ buildGrid();
 //****************** Begin code for DRAWING ******************/
 //************************************************************/
 function letsDraw() {
-	//*** As in color theory, shade adds black to the color ****/
+	//*** shade adds black to the color ************************/
 	let shade = Number(this.style.getPropertyValue('opacity'));
+
 	//*** toFixed() returns a string, so needs Number() ********/
+	//*** I used 0.9 because JS sets opacity at 0.99999 ********/
+	//*** when incrementing by 0.1 *****************************/
 	if (Number(shade.toFixed(1)) <= 0.9) {
 		shade += 0.1;
-		console.log(`Opacity set to ${shade}`);
+		// console.log(`Opacity set to ${shade}`);
 	}
 	this.style.opacity = shade;
 }
