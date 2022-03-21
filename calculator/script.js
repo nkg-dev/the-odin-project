386e32971c806352e8b23ddf59eceb78b13061c0
Neil G <95941773+nkg-dev@users.noreply.github.com>
2022-03-21 12:43:19 -0400

    most calculator functionality complete

--- calculator/script.js
+++ calculator/script.js
@@ -1,5 +1,103 @@
 'use strict';
 
+class Calculator {
+	constructor(previousOperandText, currentOperandText) {
+		this.previousOperandText = previousOperandText;
+		this.currentOperandText = currentOperandText;
+		this.clear();
+	}
+
+	clear() {
+		this.currentOperand = '';
+		this.previousOperand = '';
+		this.operation = undefined;
+	}
+
+	backSpace() {
+		this.currentOperand = this.currentOperand.toString().slice(0, -1);
+	}
+
+	appendNumber(number) {
+		if (number === '.' && this.currentOperand.includes('.')) return;
+		this.currentOperand = this.currentOperand.toString() + number.toString();
+	}
+
+	chooseOperation(operation) {
+		if (this.currentOperand === '') return;
+		if (this.previousOperand !== '') {
+			this.compute();
+		}
+		this.operation = operation;
+		this.previousOperand = this.currentOperand;
+		this.currentOperand = '';
+	}
+
+	compute() {
+		let calculation;
+		const previous = parseFloat(this.previousOperand);
+		const current = parseFloat(this.currentOperand);
+		if (isNaN(previous) || isNaN(current)) return;
+		switch (this.operation) {
+			case '+':
+				calculation = previous + current;
+				break;
+			case '-':
+				calculation = previous - current;
+				break;
+			case '×':
+				calculation = previous * current;
+				break;
+			case '÷':
+				calculation = previous / current;
+				break;
+			default:
+				return;
+		}
+		this.currentOperand = calculation;
+		this.operation = undefined;
+		this.previousOperand = '';
+	}
+
+	getDisplayNumber(number) {
+		const stringNumber = number.toString();
+		const integerDigits = parseFloat(stringNumber.split('.')[0]);
+		const decimalDigits = stringNumber.split('.')[1];
+		let integerDisplay;
+		if (isNaN(integerDigits)) {
+			integerDisplay = '';
+		} else {
+			integerDisplay = integerDigits.toLocaleString('en-US', {
+				maximumFractionDigits: 0,
+			});
+		}
+		if (decimalDigits != null) {
+			return `${integerDisplay}.${decimalDigits}`;
+		} else {
+			return integerDisplay;
+		}
+	}
+
+	updateDisplay() {
+		this.currentOperandText.textContent = this.getDisplayNumber(
+			this.currentOperand
+		);
+		if (this.operation != null) {
+			this.previousOperandText.textContent = `${this.previousOperand}${this.operation}`;
+		} else {
+			this.previousOperandText.textContent = '';
+		}
+	}
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandText = document.querySelector('[data-previous-operand]');
+const currentOperandText = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandText, currentOperandText);
 // keyvalues Return == Enter
 // BUT Clear button (on keyboards with numberpad) is Clear NOT Delete or Backspace
 
@@ -13,7 +111,6 @@ function toggleActive(e) {
 	//* Make Active
 	console.log('Add');
 	e.target.classList.add('active');
-	return;
 }
 
 function addActive(btn) {
@@ -25,12 +122,22 @@ function addActive(btn) {
 function keyPress(e) {
 	// match the button the key pressed
 	console.log(`e.key value is ${e.key}`);
-	const btn = document.querySelector(`button[data-key='${e.key}']`);
+	let keyValue = e.key;
+	switch (keyValue) {
+		case 'Enter':
+			keyValue = '=';
+			break;
+		case 'Clear':
+			keyValue = 'Delete';
+			break;
+	}
+	console.log(`keyValue is '${keyValue}'`);
+	const btn = document.querySelector(`button[data-key='${keyValue}']`);
 	// if no button matches, exit function
 	if (!btn) return;
 	// toggleActive(btn);
 	btn.classList.add('active');
-	// routeKey(e.key);
+	routeInput(keyValue);
 }
 
 // function routeKeyPress(key)
@@ -45,11 +152,45 @@ function mouseClick(e) {
 	// addActive(btn);
 	// e.target.addEventListener('transitionend', toggleActive);
 	e.target.classList.add('active');
+	routeInput(e.target.value);
 }
 
-function routeKey(key) {
-	switch (key) {
-		case '0':
+function routeInput(value) {
+	const numberArray = ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+	const operatorArray = ['+', '-', '*', '/'];
+	const functionArray = ['Backspace', 'Delete', 'Clear'];
+	console.log(`value == ${value}`);
+	console.log(typeof value);
+	if (numberArray.includes(value)) {
+		console.log(`${value} is a number`);
+		calculator.appendNumber(value);
+		calculator.updateDisplay();
+	}
+	if (operatorArray.includes(value)) {
+		console.log(`${value} is an operator`);
+		if (value === '/') {
+			value = '÷';
+		}
+		if (value === '*') {
+			value = '×';
+		}
+		calculator.chooseOperation(value);
+		calculator.updateDisplay();
+	}
+	if (value === '=') {
+		console.log(`${value} is Equals`);
+		calculator.compute();
+		calculator.updateDisplay();
+	}
+	if (value === 'Backspace') {
+		console.log(`${value} is Backspace`);
+		calculator.backSpace();
+		calculator.updateDisplay();
+	}
+	if (value === 'Delete') {
+		console.log(`${value} is Delete`);
+		calculator.clear();
+		calculator.updateDisplay();
 	}
 }
 
@@ -64,8 +205,10 @@ btns.forEach((key) =>
 
 document.addEventListener('keydown', keyPress);
 document.addEventListener('pointerdown', (e) => {
-	console.log(e.target.value);
-	toggleActive(e);
+	if (!e.target.value) return;
+	routeInput(e.target.value);
+	// toggleActive(e);
+	// mouseClick(e);
 });
 // document.addEventListener('touchstart', (e) => {
 // 	toggleActive(e);
@@ -73,9 +216,9 @@ document.addEventListener('pointerdown', (e) => {
 // document.addEventListener('touchend', (e) => {
 // 	toggleActive(e);
 // });
-document.addEventListener('pointerup', (e) => {
-	e.target.addEventListener('transitionend', toggleActive(e));
-});
+// document.addEventListener('pointerup', (e) => {
+// 	e.target.addEventListener('transitionend', toggleActive(e));
+// });
 
 //************************************************************/
 //************** Begin code for data-theme swap **************/
